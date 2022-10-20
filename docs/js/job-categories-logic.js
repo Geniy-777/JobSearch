84c14cffd49b4355d975c2b2225d60a0f10be0fb
Geniy-777 <mom_usta@mail.ru>
2022-10-20 23:34:24 +0300

    add adaptive design production

--- docs/js/job-categories-logic.js
+++
@@ -1,4 +0,0 @@
-$(() => {
-  
-});
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJqb2ItY2F0ZWdvcmllcy1sb2dpYy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIkKCgpID0+IHtcbiAgXG59KTsiXSwiZmlsZSI6ImpvYi1jYXRlZ29yaWVzLWxvZ2ljLmpzIn0=
